18520cc53307788ed471ebcf1ce32ae2f051f207
gabriela-gnsales <gnsales.gabriela@gmail.com>
2023-04-06 12:48:36 -0300

    Adicionando conteúdo de kotlin

--- kotlin/aulas.kt
+++ kotlin/aulas.kt
@@ -240,7 +240,7 @@ open class ABC {
     }
 }
 
-class BCD: ABC() {
+class BCD: ABC() { // não precisa das {} nesse caso, mas não dá erro se colocar
 }
 
 class CDE: ABC() {
@@ -259,5 +259,252 @@ fun main() {
     var b = CDE()
     b.falar()
     b.gritar()
+}
+
+// interface
+// as interfaces podem conter a implementação de métodos, bem como a declaração de métodos abstratos
+// no java isso só foi possível a partir da versão 8
+// da msm forma que no java, no kotlin não é possível instanciar diretamente uma interface, tem que criar uma classe que implementa aquela interface
+// no kotlin o 2 pontos : significa tanto extends qnt implements
+interface PessoaInterface {
+    var idade: Int
+    fun falar(): String
+    fun saudar() {
+        println("Olá, alunos")
+    }
+}
+
+class PessoaImp: PessoaInterface {
+    override var idade: Int = 25
+    override fun falar() = "Bom dia!"
+}
+
+fun main() {
+    val obj = PessoaImp()
+    println("Minha idade é ${obj.idade}")
+    print("Chamando saudar(): ")
+    obj.saudar()
+    print("Chamando falar(): ")
+    println(obj.falar())
+}
+
+interface A {
+    fun mensagemA() {
+        println("Método da interface A")
+    }
+}
+
+interface B {
+    fun mensagemB() {
+        println("Método da interface B")
+    }
+}
+
+class ExemploMultiplasInterfaces: A, B
+
+fun main() {
+    val obj = ExemploMultiplasInterfaces()
+    obj.mensagemA()
+    obj.mensagemB()
+}
+
+/*
+Modificadores de Visibilidade
+- há quatro modificadores de visibilidade em Kotlin: Private, Protected, Internal, Public
+- a visibilidade padrão é public
+- no java, a visibilidade padrão é protected
+- private significa visível apenas dentro desta classe (incluindo todos os seus membros);
+- protected é o mesmo que private mas também é visível nas subclasses;
+- internal significa que qualquer cliente dentro deste módulo que vê a classe declarante vê seus membros internal
+- o modificador internal de visibilidade significa que o membro está visível dentro do mesmo módulo (um módulo é um conjunto de arquivos Kotlin compilados juntos)
+- public significa que qualquer cliente que vê a classe declarante vê seus membros public
+- no Kotlin, a classe externa não vê membros privados de suas classes internas
+- se você substituir um membro protected e não especificar a visibilidade explicitamente, o membro que está substituindo também terá visibilidade protected
+- por padrão, todos os construtores são public , o que efetivamente significa que eles estão visíveis em todos os lugares onde a classe está visível (isso significa que um construtor de uma classe internal só é visível dentro do mesmo módulo)
+- variáveis, funções e classes locais não podem ter modificadores de visibilidade
+*/
+
+// pacotes
+package	foo
+
+fun baz() {
+    println("baz")
+}
+
+class Bar
+
+fun main() {
+    var b: Bar
+    baz()
+}
+
+// arquivo: exemplo.kt
+package foo
+
+private fun foo() { print("Olá") } // visível dentro de exemplo.kt
+
+public var bar: Int = 5 // propriedade é visível em todo lugar
+	private set // setter é visível apenas em exemplo.kt
+
+internal val baz = 6 // visível dentro do mesmo módulo
+
+open class Externa {
+    private	val a = 1
+    protected open val b = 2
+    internal val c = 3
+    val d = 4 // public por padrão
+    
+    protected class Aninhada {
+        public val e: Int = 5
+    }
+}
+
+class Subclasse: Externa() {
+    // a não é visível
+    // b, c, d são visíveis
+    // Aninhada, e são visíveis
+    override val b = 5 // b é protected
+}
+
+class NaoRelacionada(o: Externa) {
+    // o.a, o.b não são visíveis
+    // o.c, o.d são visíveis (mesmo módulo)
+    // Externa.Aninhada não é visível, e Aninhada::e não é visível também
+}
+
+// extensões de função
+fun MutableList<Int>.troca(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
+}
+
+fun main() {
+    val list = mutableListOf(1, 2, 3)
+    println(list)
+    list.troca(0, 2)
+    println(list)
+}
+
+// extensões de objeto
+class A {
+    companion object {
+        fun saudar(): String {
+            return("Bom dia!") // pode ser tbm só: return "Bom dia!"
+        }
+    }
+}
+
+fun main() {
+    println("Olá " + A.saudar())
+}
+
+// classes de dados
+data class Livro(val nome: String, val editora: String, var nota: Int)
+
+fun main() {
+    val livro: Livro = Livro("Kotlin", "Gran Cursos", 5) // pode ser tbm: val livro = Livro("Kotlin", "Gran Cursos", 5) -> sem informar o tipo de dado "Livro"
+    println("O nome do livro é: " + livro.nome)
+    println("Editora: " + livro.editora)
+    println("Nota: " + livro.nota)
+    livro.nota = 7
+    println("Imprimindo tudo: " + livro.toString())
+    println("HashCode: " + livro.hashCode())
+}
+
+// classes seladas não podem ser estendidas depois que foram compiladas
+
+sealed interface Expr
+
+sealed class MathExpr(): Expr
+
+data class Const(val number: Double): MathExpr()
+data class Sum(val e1: Expr, val e2: Expr): MathExpr()
+object NotANumber: Expr
+
+// genéricos
+class Caixa<T>(t: T) {
+    var valor = t
+}
+
+val caixa1: Caixa<Int> = Caixa<Int>(1) // pode ser tbm: val caixa1: Caixa<Int> = Caixa(1)
+val caixa2 = Caixa(2)
+
+fun main() {
+    println(caixa1.valor)
+    println(caixa2.valor)
+}
+
+
+????????????
+
+// função sem retorno -> Unit
+fun printHello(name: String?): Unit { // pode ser tbm sem o Unit: fun printHello(name: String?) {
+    if (name != null)
+    	println("Hello, $name!")
+    else
+    	println("Hi there!")
+    // 'return Unit' ou 'return' é opcional
+}
+
+fun main() {
+    var a = printHello("Gabi")
+    var b = printHello(null)
+}
+
+// funções de expressão única
+fun teste1(x: Int): Int = x * 2
+fun teste2(x: Int = 5) = x * 2
+fun teste3(x: Int = 5) = x * 2
+
+fun main() {
+    var a = teste1(2)
+    var b = teste2(2)
+    var c = teste3()
+    println("a = $a \nb = $b \nc = $c")
+}
+
+// número variável de argumentos
+fun <T> asList(vararg ts: T): List<T> {
+    val result = ArrayList<T>()
+    for (t in ts) // ts é um Array
+    	result.add(t)
+    return result
+}
+
+fun main() {
+    val a = asList(1, 2, 3)
+    val b = arrayOf(1, 2, 3)
+    val c = asList(-1, 0, 4)
+    
+    println("a = $a \nb = $b \nc = $c")
+}
+
+// notação infixa
+infix fun Int.sh1(x: Int): Int { return x + 1 }
+
+fun main() {
+    // chamando a função usando a notação infixa
+    println(1 sh1 2)
+
+    // é o mesmo que
+    println(1.sh1(2))
+}
+
+
+
+
+
+// SLIDE 36
+
+
+// TESTE
+
+fun main() {
+    val a = 5
+    var b = ++a
+    
+    println("a = $a")
+    println("b = $b")
 }
 
