52f2ef1d574cbeb1a2b9fc7562f698bd914391dc
gabriela-gnsales <gnsales.gabriela@gmail.com>
2023-04-09 15:53:17 -0300

    Adicionando conteúdo de swift

--- swift/aulas.swift
+++ swift/aulas.swift
@@ -86,5 +86,132 @@ for nome in nomes[...2] {
   print(nome)
 }
 
+// switch
 
+var a = 10
+switch a {
+  case 100:
+    print("valor é 100")
+  case 10, 15:
+    print("valor 10 ou 15")
+    fallthrough // verifica as demais condições, imprimindo "valor 10 ou 15" e "valor entre 10 e 15" para a = 10 e a = 15
+  case 10...15:
+    print("valor entre 10 e 15")
+  default:
+    print("caso padrão")
+}
+
+// arrays
+
+// sintaxe convencional
+var a: Array<Int> = [1, 5]
+print(a)
+
+// sintaxe abreviada
+var b: [Int]
+b = [2, 4, 8]
+print("b possui \(b.count) elementos")
+
+// tipo inferido
+var c = [6, 8]
+c.append(3)
+print("c: \(c)")
+
+c = []
+print("o tipo se mantém:", c, type(of: c))
+
+var tresValores = Array(repeating: 1.2, count: 3)
+print("tresValores:", tresValores)
+print("tipo tresValores:", type(of: tresValores))
+
+// Conjuntos / Sets -> elementos não são duplicados e nem indexados (não dá pra saber a posição deles)
+
+var a: Set<Int> = [1, 5, 1]
+print(a)
+// [5, 1]
+
+// Dicionários -> não perde a memória do tipo original
+
+var nomesDeInteiros: [Int: String] = [:]
+nomesDeInteiros[16] = "dezesseis"
+print(nomesDeInteiros)
+nomesDeInteiros = [:]
+print(nomesDeInteiros, type(of: nomesDeInteiros))
+
+var ufs: [String: String] = ["GO": "Goiás", "DF": "Brasília"]
+
+// opcional
+if let uf = ufs["DF"] {
+  print("O nome da UF é \(uf).")
+} else {
+  print("UF não está no dicionário")
+}
+
+// removendo
+ufs["GO"] = nil
+print(ufs)
+
+if let removido = ufs.removeValue(forKey: "DF") {
+  print("O valor removido é \(removido).")
+} else {
+  print("O dicionário não continha a chave.")
+}
+print(ufs)
+
+// funções
+
+// definição
+func saudar(pessoa: String) -> String {
+  let saudacao = "Olá, " + pessoa + "!"
+  return saudacao
+}
+
+// chamada
+print(saudar(pessoa: "Gabi"))
+
+// sem retorno
+func saudar2(pessoa: String) {
+  print("Olá, " + pessoa + "!")
+}
+saudar2(pessoa: "Jhony")
+
+// retorno implícito
+func saudar3(para pessoa: String) -> String {
+  "Olá, " + pessoa + "!"
+}
+print(saudar3(para: "Maria"))
+
+// omitindo rótulo
+func saudar4(_ pessoa: String) -> String {
+  "Olá, " + pessoa + "!"
+}
+print(saudar4("Maria"))
+
+// parâmetros padrão
+func saudar5(pessoa: String = "anônimo") {
+  print("Olá, " + pessoa + "!")
+}
+saudar5()
+
+// parâmetros variáveis
+func media(_ numeros: Double...) -> Double {
+  var total: Double = 0
+  for num in numeros {
+    total += num
+  }
+  return total / Double(numeros.count)
+}
+print(media(1, 2, 3, 4, 5))
+
+// inout -> parâmetros de entrada são os mesmos de saída
+func trocaValores(_ a: inout Int, _ b: inout Int) {
+  let tempA = a
+  a = b
+  b = tempA
+}
+var umInt = 3
+var outroInt = 27
+trocaValores(&umInt, &outroInt)
+
+print("umInt:", umInt, "\noutroInt:", outroInt)
 
